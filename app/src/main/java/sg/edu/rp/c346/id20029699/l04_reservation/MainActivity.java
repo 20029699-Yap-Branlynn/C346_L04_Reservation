66730a9a12b61aa0f81067fc071d4830b7b5d9c8
Yap Branlynn <20029699@myrp.edu.sg>
2022-05-12 15:33:28 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20029699/l04_reservation/MainActivity.java
@@ -0,0 +1,78 @@
+package sg.edu.rp.c346.id20029699.l04_reservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText name;
+    EditText number;
+    EditText pax;
+    DatePicker dp;
+    TimePicker tp;
+    CheckBox cb;
+    Button confirm;
+    Button reset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.editTextName);
+        number = findViewById(R.id.editTextPhoneNum);
+        pax = findViewById(R.id.editTextPax);
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        cb = findViewById(R.id.checkBoxSmoking);
+        confirm = findViewById(R.id.buttonConfirm);
+        reset = findViewById(R.id.buttonReset);
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int hour = tp.getCurrentHour();
+                int min = tp.getCurrentMinute();
+                int day = dp.getDayOfMonth();
+                int month = dp.getMonth();
+                int year = dp.getYear();
+
+                if (cb.isChecked() == true){
+                    String msg = name + " Your reservation for "
+                            + pax + " on " + day+ "/" + month + "/" + year + " at " + hour + ":" + min +
+                            " in the smoking area has been confirmed. See you soon!";
+
+                    Toast.makeText(MainActivity.this, "Reservation in smoking area Confirmed", Toast.LENGTH_LONG).show();
+                }else {
+                    String msg = name + " Your reservation for "
+                            + pax + " on " + day+ "/" + month + "/" + year + " at " + hour + ":" + min +
+                            " has been confirmed. See you soon!";
+
+                    Toast.makeText(MainActivity.this, "Reservation Confirmed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name.setText("");
+                number.setText("");
+                pax.setText("");
+                cb.setChecked(false);
+                dp.updateDate(2020, 5, 1);
+                tp.setCurrentHour(19);
+                tp.setCurrentMinute(30);
+            }
+        });
+
+    }
+}
